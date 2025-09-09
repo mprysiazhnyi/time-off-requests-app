28f4c04526f098664c1b510ba38adb3c9b344e5e
Maksym Prysiazhnyi <maksym.prysiazhnyi@valatron.com>
2025-09-09 11:16:24 +0200

    Update base

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from "vitest/config";
 // @ts-ignore
 import react from "@vitejs/plugin-react";
-
+import { loadEnv } from "vite";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts", // optional
-  },
-  server: {
-    port: 5173,
-  },
-  base: process.env.VITE_BASE,
-});
+// @ts-ignore
+export default ({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts", // optional
+    },
+    server: {
+      port: 5173,
+    },
+    base: env.VITE_BASE,
+  });
+};
